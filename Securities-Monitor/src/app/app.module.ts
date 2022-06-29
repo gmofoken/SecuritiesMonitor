8de5a58b5ce689d3b66671b91bd843eaf04e7387
Gomotso Mofokeng <Gomotso.Mofokeng@psg.co.za>
2022-06-29 12:50:16 +0200

    Added candle stick chart

--- Securities-Monitor/src/app/app.module.ts
+++ Securities-Monitor/src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgApexchartsModule } from "ng-apexcharts";
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgApexchartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
